91f616df053757c6c584be4bbee332d2d801c537
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2021-04-11 22:16:00 +0200

    Add a JupyterLab Classic frontend

---
+++ app-classic/index.js
@@ -0,0 +1,125 @@
+// Copyright (c) Jupyter Development Team.
+// Distributed under the terms of the Modified BSD License.
+
+import { JupyterLiteServer } from '@jupyterlite/server';
+
+// The webpack public path needs to be set before loading the CSS assets.
+import { PageConfig } from '@jupyterlab/coreutils';
+// eslint-disable-next-line
+__webpack_public_path__ = PageConfig.getOption('fullStaticUrl') + '/';
+
+require('./build/style.js');
+
+const serverMods = [
+  import('@jupyterlite/javascript-kernel-extension'),
+  import('@jupyterlite/pyodide-kernel-extension'),
+  import('@jupyterlite/server-extension')
+];
+
+window.addEventListener('load', async () => {
+  // create the in-browser JupyterLite Server
+  const jupyterLiteServer = new JupyterLiteServer({});
+  jupyterLiteServer.registerPluginModules(await Promise.all(serverMods));
+  // start the server
+  await jupyterLiteServer.start();
+
+  // retrieve the custom service manager from the server app
+  const { serviceManager } = jupyterLiteServer;
+
+  // create a JupyterLab Classic frontend
+  const { App, ClassicShell } = require('@jupyterlab-classic/application');
+  // TODO: remove explicit shell instantiation when fixed in jupyterlab-classic
+  const app = new App({ shell: new ClassicShell() });
+  // TODO: remove when fixed upstream
+  app.serviceManager = serviceManager;
+
+  let mods = [
+    // @jupyterlite plugins
+    require('@jupyterlite/application-extension'),
+    require('@jupyterlite/theme-extension'),
+    // @jupyterlab-classic plugins
+    require('@jupyterlab-classic/application-extension'),
+    require('@jupyterlab-classic/docmanager-extension'),
+    require('@jupyterlab-classic/help-extension'),
+    require('@jupyterlab-classic/notebook-extension'),
+    // to handle opening new tabs after creating a new terminal
+    require('@jupyterlab-classic/terminal-extension'),
+
+    // @jupyterlab plugins
+    require('@jupyterlab/apputils-extension').default.filter(({ id }) =>
+      [
+        '@jupyterlab/apputils-extension:palette',
+        '@jupyterlab/apputils-extension:settings',
+        '@jupyterlab/apputils-extension:themes',
+        '@jupyterlab/apputils-extension:themes-palette-menu'
+      ].includes(id)
+    ),
+    require('@jupyterlab/codemirror-extension').default.filter(({ id }) =>
+      [
+        '@jupyterlab/codemirror-extension:services',
+        '@jupyterlab/codemirror-extension:codemirror'
+      ].includes(id)
+    ),
+    require('@jupyterlab/completer-extension').default.filter(({ id }) =>
+      ['@jupyterlab/completer-extension:manager'].includes(id)
+    ),
+    require('@jupyterlab/docmanager-extension').default.filter(({ id }) =>
+      ['@jupyterlab/docmanager-extension:plugin'].includes(id)
+    ),
+    require('@jupyterlab/mainmenu-extension'),
+    require('@jupyterlab/mathjax2-extension'),
+    require('@jupyterlab/notebook-extension').default.filter(({ id }) =>
+      [
+        '@jupyterlab/notebook-extension:factory',
+        '@jupyterlab/notebook-extension:tracker',
+        '@jupyterlab/notebook-extension:widget-factory'
+      ].includes(id)
+    ),
+    require('@jupyterlab/rendermime-extension'),
+    require('@jupyterlab/shortcuts-extension'),
+    // so new terminals can be create from the menu
+    require('@jupyterlab/terminal-extension'),
+    require('@jupyterlab/theme-light-extension'),
+    require('@jupyterlab/theme-dark-extension')
+  ];
+
+  // The motivation here is to only load a specific set of plugins dependending on
+  // the current page
+  const page = PageConfig.getOption('classicPage');
+  if (page === 'tree') {
+    mods = mods.concat([
+      require('@jupyterlab-classic/tree-extension'),
+      require('@jupyterlab/running-extension')
+    ]);
+  } else if (page === 'notebooks') {
+    mods = mods.concat([
+      require('@jupyterlab/completer-extension').default.filter(({ id }) =>
+        ['@jupyterlab/completer-extension:notebooks'].includes(id)
+      ),
+      require('@jupyterlab/tooltip-extension').default.filter(({ id }) =>
+        [
+          '@jupyterlab/tooltip-extension:manager',
+          '@jupyterlab/tooltip-extension:notebooks'
+        ].includes(id)
+      )
+    ]);
+  } else if (page === 'edit') {
+    mods = mods.concat([
+      require('@jupyterlab/completer-extension').default.filter(({ id }) =>
+        ['@jupyterlab/completer-extension:files'].includes(id)
+      ),
+      require('@jupyterlab/fileeditor-extension').default.filter(({ id }) =>
+        ['@jupyterlab/fileeditor-extension:plugin'].includes(id)
+      ),
+      require('@jupyterlab-classic/tree-extension').default.filter(({ id }) =>
+        ['@jupyterlab-classic/tree-extension:factory'].includes(id)
+      )
+    ]);
+  }
+
+  app.registerPluginModules(mods);
+
+  console.log('Starting app');
+  await app.start();
+  console.log('JupyterLite Classic started, waiting for restore');
+});
