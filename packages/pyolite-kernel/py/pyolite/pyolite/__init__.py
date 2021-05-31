6bdf4f184de3754ff1392e1861fecabdb0ba9094
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2021-05-31 09:13:08 +0200

    Set recursion limit

--- packages/pyolite-kernel/py/pyolite/pyolite/__init__.py
+++ packages/pyolite-kernel/py/pyolite/pyolite/__init__.py
@@ -2,7 +2,12 @@
 
 __version__ = "0.1.0"
 
+import sys
 from .patches import ensure_matplotlib_patch
+
+# Set the recursion limit, needed for altair
+# for more details, see: https://github.com/jtpio/jupyterlite/pull/113#issuecomment-851072065
+sys.setrecursionlimit(max(170, sys.getrecursionlimit()))
 
 # apply patches for available modules
 ensure_matplotlib_patch()
