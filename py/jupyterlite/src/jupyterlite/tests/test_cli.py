4ccedc15c6c45f4a5a13fca92d5f0aa1557959e6
Nicholas Bollweg <nick.bollweg@gmail.com>
2021-06-17 10:32:36 -0400

    more cli testing

--- py/jupyterlite/src/jupyterlite/tests/test_cli.py
+++ py/jupyterlite/src/jupyterlite/tests/test_cli.py
@@ -26,7 +26,7 @@ NOT_SERVE_HOOK = [h for h in HOOKS if h != "serve"]
 
 @pytest.mark.parametrize("lite_args", LITE_INVOCATIONS)
 @pytest.mark.script_launch_mode("subprocess")
-def test_cli_version(script_runner, lite_args):
+def test_cli_version(lite_args, script_runner):
     """do various invocations work"""
     returned_version = script_runner.run(*lite_args, "--version")
     assert returned_version.success
@@ -34,9 +34,19 @@ def test_cli_version(script_runner, lite_args):
     assert returned_version.stderr == ""
 
 
+@pytest.mark.parametrize("lite_args", LITE_INVOCATIONS)
+@pytest.mark.parametrize("help", ["-h", "--help"])
+@pytest.mark.script_launch_mode("subprocess")
+def test_cli_help(lite_args, help, script_runner):
+    """does help work"""
+    returned_version = script_runner.run(*lite_args, help)
+    assert returned_version.success
+    assert returned_version.stderr == ""
+
+
 @pytest.mark.script_launch_mode("subprocess")
 @pytest.mark.parametrize("lite_hook", ["list", "status"])
-def test_cli_status_null(an_empty_lite_dir, script_runner, lite_hook):
+def test_cli_status_null(lite_hook, an_empty_lite_dir, script_runner):
     """do the "side-effect-free" commands create exactly one file?"""
     returned_status = script_runner.run(
         "jupyter", "lite", lite_hook, cwd=an_empty_lite_dir
@@ -51,7 +61,8 @@ def test_cli_status_null(an_empty_lite_dir, script_runner, lite_hook):
 
 @pytest.mark.script_launch_mode("subprocess")
 @pytest.mark.parametrize("lite_hook", NOT_SERVE_HOOK)
-def test_cli_check(an_empty_lite_dir, script_runner, lite_hook):
+def test_cli_any_hook(lite_hook, an_empty_lite_dir, script_runner):
+    """does all the hooks basically work"""
     expected_files = TRASH if lite_hook in FAST_HOOKS else A_GOOD_BUILD
     started = time.time()
     returned_status = script_runner.run(
@@ -64,25 +75,46 @@ def test_cli_check(an_empty_lite_dir, script_runner, lite_hook):
     for expected in expected_files:
         assert (an_empty_lite_dir / expected).exists()
 
-    if lite_hook not in FAST_HOOKS:
-        # re-run, be faster
-        restarted = time.time()
-        rereturned_status = script_runner.run(
-            "jupyter", "lite", lite_hook, cwd=an_empty_lite_dir
-        )
-        duration_2 = time.time() - restarted
-        assert rereturned_status.success
-        assert duration_1 > duration_2
+    if lite_hook in FAST_HOOKS:
+        return
 
-        # force
-        forced_status = script_runner.run(
-            "jupyter", "lite", lite_hook, "--force", cwd=an_empty_lite_dir
-        )
+    # re-run, be faster
+    restarted = time.time()
+    rereturned_status = script_runner.run(
+        "jupyter", "lite", lite_hook, cwd=an_empty_lite_dir
+    )
+    duration_2 = time.time() - restarted
+    assert rereturned_status.success
+    assert duration_1 > duration_2
+
+    # force, detect files
+    readme = an_empty_lite_dir / "README.md"
+    readme.write_text("# hello world", encoding="utf-8")
+
+    overrides = an_empty_lite_dir / "overrides.json"
+    overrides.write_text("{}", encoding="utf-8")
+
+    forced_status = script_runner.run(
+        "jupyter",
+        "lite",
+        lite_hook,
+        "--force",
+        "--files",
+        readme,
+        "--overrides",
+        overrides,
+        cwd=an_empty_lite_dir,
+    )
+
+    if A_GOOD_BUILD[-1] in expected_files and lite_hook not in ["init"]:
+        print(sorted(an_empty_lite_dir.rglob("_output")))
+        assert (an_empty_lite_dir / "_output/files/README.md").exists()
         assert forced_status.success
 
 
 @pytest.mark.script_launch_mode("subprocess")
 def test_cli_raw_doit(an_empty_lite_dir, script_runner):
+    """does raw doit work"""
     returned_status = script_runner.run(
         "jupyter", "lite", "doit", "--", "--help", cwd=an_empty_lite_dir
     )
