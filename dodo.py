c1ca877526b64576348b41b138d966f7922ab37d
Nicholas Bollweg <nick.bollweg@gmail.com>
2021-06-23 23:52:43 -0400

    move typedoc to lint

--- dodo.py
+++ dodo.py
@@ -107,7 +107,7 @@ def task_lint():
 
 def task_build():
     """build code and intermediate packages"""
-    if C.TESTING_IN_CI:
+    if C.TESTING_IN_CI or C.DOCS_IN_CI:
         return
 
     if not (C.RTD or C.CI):
@@ -193,9 +193,6 @@ def task_build():
 
     app_deps = [B.META_BUILDINFO, P.WEBPACK_CONFIG, P.LITE_ICON, P.LITE_WORDMARK]
     all_app_targets = []
-
-    if C.DOCS_IN_CI:
-        return
 
     for app_json in P.APP_JSONS:
         app = app_json.parent
@@ -333,7 +330,7 @@ def task_dev():
 
 def task_docs():
     """build documentation"""
-    if C.LINTING_IN_CI or C.TESTING_IN_CI or C.BUILDING_IN_CI:
+    if C.TESTING_IN_CI or C.BUILDING_IN_CI:
         return
 
     yield dict(
