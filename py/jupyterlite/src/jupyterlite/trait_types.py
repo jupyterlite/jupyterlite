67b87638ea0a94c494b42b9b64b116e463633c20
Nicholas Bollweg <nick.bollweg@gmail.com>
2021-06-30 19:44:57 -0400

    move more path logic to traitlets

--- py/jupyterlite/src/jupyterlite/trait_types.py
+++ py/jupyterlite/src/jupyterlite/trait_types.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from traitlets import TraitType
+from traitlets import Container, TraitType
 
 
 class CPath(TraitType):
@@ -8,9 +8,19 @@ class CPath(TraitType):
 
     def validate(self, obj, value) -> Path:
         if isinstance(value, Path):
-            return value
+            return value.resolve()
 
         try:
-            return Path(str(value))
+            return Path(str(value)).resolve()
         except Exception:
             self.error(obj, value)
+
+
+class TypedTuple(Container):
+    """A trait for a tuple of any length with type-checked elements.
+
+    From: https://github.com/jupyter-widgets/ipywidgets/blob/7.6.3/ipywidgets/widgets/trait_types.py#L213
+    """
+
+    klass = tuple
+    _cast_types = (list,)
