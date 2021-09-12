6bf753d666ffae024bcbc379a552421f0558cfa9
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2021-09-12 14:12:02 +0200

    Copy py assets to dist on release

--- dodo.py
+++ dodo.py
@@ -342,7 +342,7 @@ def task_dist():
         dest = B.DIST / dist.name
         py_dests += [dest]
         yield dict(
-            name=f"copy:{dist.name}",
+            name=f"copy:py:{dist.name}",
             actions=[(U.copy_one, [dist, dest])],
             file_dep=[dist],
             targets=[dest],
