2819ae4728e454ba7b2da732c24c5470bb26ba27
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2021-09-28 15:13:38 +0200

    Add comment about the upstream plugin

--- packages/translation/src/translation.ts
+++ packages/translation/src/translation.ts
@@ -3,6 +3,11 @@ import { PageConfig, URLExt } from '@jupyterlab/coreutils';
 import { JSONObject } from '@lumino/coreutils';
 
 /**
+ * A fake locale to retrieve all the language packs.
+ */
+const ALL = 'all';
+
+/**
  * A class to handle requests to /api/translations
  */
 export class Translation {
@@ -19,8 +24,11 @@ export class Translation {
     try {
       const response = await fetch(apiURL);
       const json = JSON.parse(await response.text());
-      if (this._prevLocale !== 'all' && locale === 'all') {
+      if (this._prevLocale !== ALL && locale === ALL) {
         // TODO: fix this logic upstream?
+        // the upstream translation plugin relies on the comparison between
+        // the display name and the native name to enable or disable the commands:
+        // https://github.com/jupyterlab/jupyterlab/blob/befa831ffef36321b87f352a48fbe2439df6c872/packages/translation-extension/src/index.ts#L117
         const prev = this._prevLocale;
         json.data[prev].displayName = json.data[prev].nativeName;
         if (prev !== 'en') {
