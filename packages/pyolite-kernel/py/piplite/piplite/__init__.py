82f77a57f73200071e8408ed6335f532b6e8fa03
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2022-01-12 14:03:26 +0000

    Publish 0.1.0a19
    SHA256 hashes:
    
    SHA256SUMS: 867b30353798b3d20f8e231ce78fd61e0d3e0e10ab004321f8b451ed989015e9
    
    jupyterlite-0.1.0a19-py3-none-any.whl: a08b873cd322f8460d0a2b23bf5b7edec01c2a73ea53d32be1f44c587003b8a5
    
    jupyterlite-0.1.0a19.tar.gz: b89f707b6838c17ba2bae550fd5bd2f66460972c7dd39a3049406d9094a8ffd3
    
    jupyterlite-app-0.1.0-alpha.19.tgz: 5e35694087b6a65d96361f74d4316fd59a15ea273a77a5682976955e55c20856
    
    jupyterlite-application-extension-0.1.0-alpha.19.tgz: f008270dd836c9d3e3ba98e64546e7c3f58c4b24d45ea9372c96adecb43fc5d9
    
    jupyterlite-contents-0.1.0-alpha.19.tgz: c7fcb067ffdd70ca486d4c28fe3f75db521ba0ccb55fccd0079cb6c1bf68a27c
    
    jupyterlite-iframe-extension-0.1.0-alpha.19.tgz: c950064735fa39d2e13d020b6c2b4ad6a3103180fe75ddfecd284def2c0198d5
    
    jupyterlite-javascript-kernel-0.1.0-alpha.19.tgz: aa6848a7b3032e0886f4ab30b5930bba1d1563b205359f5fa97617d7ea3a626f
    
    jupyterlite-javascript-kernel-extension-0.1.0-alpha.19.tgz: 738f65d8836878dbd8cb1df3f35b420e0fd976c1484261d3c787a207d3862f09
    
    jupyterlite-kernel-0.1.0-alpha.19.tgz: 335123e1fd952ac3e861965db8ca8ffd00fb7705c9e64c4a074507908a91e600
    
    jupyterlite-pyolite-kernel-0.1.0-alpha.19.tgz: aaf1e8f5bd69668299d053261392be96a7cc284224ec1c8f889a72eedeee6194
    
    jupyterlite-pyolite-kernel-extension-0.1.0-alpha.19.tgz: 43819fd2722ef316c0be52d934734b78206c47b9ffa1a40b1c677c893e14e0b7
    
    jupyterlite-retro-application-extension-0.1.0-alpha.19.tgz: 4c92fbad50b92bdc41c8fd91f24d6ed857ecc42b36dd4b00d51d7a033288b3bd
    
    jupyterlite-server-0.1.0-alpha.19.tgz: 0cd64d2bf14814cf8cf7294904460abe4c7feb2d700692a45918acf68592b529
    
    jupyterlite-server-extension-0.1.0-alpha.19.tgz: f7da6ad6836e57682d4d803fc62717ac060fcd8785c4e5953f78a0d9785c025d
    
    jupyterlite-session-0.1.0-alpha.19.tgz: b8c741da64a6e5d1cfc3d7f554a0ae5a02427cbabb78bea1963930ef1255b082
    
    jupyterlite-settings-0.1.0-alpha.19.tgz: fbe4595e3a93b53465295d009c4f31729c04456ca2a6f5ee6e0085c8ca8b1b16
    
    jupyterlite-translation-0.1.0-alpha.19.tgz: 81323f24b50057fecf97959c39761eb043d12f78db2a1520c5f7e0090e3f5776
    
    jupyterlite-ui-components-0.1.0-alpha.19.tgz: ab6ebcfea2340ca0e7667c85f79aaae04ec802c41deb5217fc9c88e3af8d920c

--- packages/pyolite-kernel/py/piplite/piplite/__init__.py
+++ packages/pyolite-kernel/py/piplite/piplite/__init__.py
@@ -1,6 +1,6 @@
 """A configurable Python package backed by Pyodide's micropip"""
 from .piplite import install
 
-__version__ = "0.1.0a18"
+__version__ = "0.1.0a19"
 
 __all__ = ["install"]
--- packages/pyolite-kernel/py/pyolite/pyolite/__init__.py
+++ packages/pyolite-kernel/py/pyolite/pyolite/__init__.py
@@ -1,6 +1,6 @@
 """A Python kernel backed by Pyodide"""
 
-__version__ = "0.1.0a18"
+__version__ = "0.1.0a19"
 
 import sys
 
