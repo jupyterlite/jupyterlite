cc7b0a0682e7b7d8ccbca80d520acde318c34b05
Nicholas Bollweg <nick.bollweg@gmail.com>
2022-02-10 11:15:30 -0600

    fix code reference to (lite)apps

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -12,7 +12,7 @@ const baseConfig = require('@jupyterlab/builder/lib/webpack.config.base');
 
 const topLevelData = require('./package.json');
 
-const liteAppData = topLevelData.jupyterlite.liteApps.reduce(
+const liteAppData = topLevelData.jupyterlite.apps.reduce(
   (memo, app) => ({ ...memo, [app]: require(`./${app}/package.json`) }),
   {}
 );
