b5dedfa3184d1fb2998e9090b639d8cc0efbafd6
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2022-02-13 11:06:40 +0100

    Add paths plugin

--- packages/console-extension/src/index.ts
+++ packages/console-extension/src/index.ts
@@ -5,6 +5,8 @@ import { JupyterFrontEndPlugin, JupyterFrontEnd } from '@jupyterlab/application'
 
 import { ITranslator } from '@jupyterlab/translation';
 
+import { SingleWidgetApp } from '@jupyterlite/application';
+
 /**
  * Add a code console to the main area on startup
  */
@@ -18,6 +20,21 @@ const consolePlugin: JupyterFrontEndPlugin<void> = {
   }
 };
 
-const plugins: JupyterFrontEndPlugin<any>[] = [consolePlugin];
+/**
+ * The default paths for a RetroLab app.
+ */
+const paths: JupyterFrontEndPlugin<JupyterFrontEnd.IPaths> = {
+  id: '@jupyterlite/console-extension:paths',
+  autoStart: true,
+  provides: JupyterFrontEnd.IPaths,
+  activate: (app: JupyterFrontEnd): JupyterFrontEnd.IPaths => {
+    if (!(app instanceof SingleWidgetApp)) {
+      throw new Error(`${paths.id} must be activated in SingleWidgetApp.`);
+    }
+    return app.paths;
+  }
+};
+
+const plugins: JupyterFrontEndPlugin<any>[] = [consolePlugin, paths];
 
 export default plugins;
