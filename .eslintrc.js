13ac705fca5375d9af9d193f3aca3558ebeff3e3
Nicholas Bollweg <nick.bollweg@gmail.com>
2022-02-13 13:16:41 -0600

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,7 +17,7 @@ module.exports = {
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'tsconfig.eslint.json',
+    project: 'tsconfigbase.json',
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint', 'jest'],
