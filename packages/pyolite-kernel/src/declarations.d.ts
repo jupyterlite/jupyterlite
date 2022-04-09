232f408ba3a3140f396043c03870a27f6dcb907c
Brendan Powers <brendan0powers@gmail.com>
2022-04-09 15:40:20 -0400

    Remove type defenitions for WindowOrWorkerGlobalScope after typescript update

--- packages/pyolite-kernel/src/declarations.d.ts
+++ packages/pyolite-kernel/src/declarations.d.ts
@@ -14,12 +14,3 @@ declare let _pipliteUrls: string[];
 declare let _disablePyPIFallback: boolean;
 declare let pyodide: any;
 declare let loadPyodide: any;
-
-// eslint-disable-next-line @typescript-eslint/naming-convention
-interface WindowOrWorkerGlobalScope {
-  // Add crossOriginIsolation to the global scope. This variable indicates
-  // the correct CORS headers are set to enable the use of SharedArrayBuffer.
-  // Details: https://developer.mozilla.org/en-US/docs/Web/API/crossOriginIsolated
-  // Remove this once we upgrade to typescript >= 4.4
-  crossOriginIsolated: boolean | undefined;
-}
