bc9a53c38a082f1623d91039b93c6049b956267e
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2022-05-04 14:00:24 +0000

    Publish 0.1.0b7
    SHA256 hashes:
    
    SHA256SUMS: 90d39fbe9af47e336e602653f841111fe517d844b9791d7af067dc888c2ea9f3
    
    jupyterlite-0.1.0b7-py3-none-any.whl: 31edc95b633ab5074be029d436a7cfbda64c54730c9f8fa7b1dc05569bece4c9
    
    jupyterlite-0.1.0b7.tar.gz: 6143018c41b43b08b597d1197586dc8663a49238f1507f41dccf3b9a30577171
    
    jupyterlite-app-0.1.0-beta.7.tgz: bff17a512e9e36829a896962dfde808dafcc35be0aa73a3de55a294397aa85aa
    
    jupyterlite-application-0.1.0-beta.7.tgz: b0c3b688633459b8bda9fa1bd8ae893dc3b4c4f3bdf7a359a71e7a2698dc668c
    
    jupyterlite-application-extension-0.1.0-beta.7.tgz: 2087020b1c8a24e20ee95b042a889efd1f00b1b02173359155cdee177e2f4117
    
    jupyterlite-contents-0.1.0-beta.7.tgz: 1685b67418c1955c599732c31b8fabc3a6324049379a8f546896196a47288386
    
    jupyterlite-iframe-extension-0.1.0-beta.7.tgz: 3565a5b8d4c0e83c6c198092530efd06320f82c6107ea7e92e1055a6511f4701
    
    jupyterlite-javascript-kernel-0.1.0-beta.7.tgz: f341137178f18e92a5194c2660f4d29c18cefbec7c3333e97d25ceff8994d65f
    
    jupyterlite-javascript-kernel-extension-0.1.0-beta.7.tgz: 54bcfccbbb50b1c45a6bcc26143ca4260592ab91f2d2f5374fce325ec4943b91
    
    jupyterlite-kernel-0.1.0-beta.7.tgz: 89ac16e11437f9cb4d9de2828b65e71fdcf8faaa0145b4887127493085bcd4a5
    
    jupyterlite-licenses-0.1.0-beta.7.tgz: 5ae17e27c5d1455954a96af83500530344c3ef23abcc44b905aba506ffe4a7d8
    
    jupyterlite-localforage-0.1.0-beta.7.tgz: fc1903218cb2e7fb94bbed4b723227f1710479c703a569b4a46059b066ecd835
    
    jupyterlite-pyolite-kernel-0.1.0-beta.7.tgz: e76b9186ca4290069219f341e8a25aefa9e6e7d23a01708a2760f1098b31bd86
    
    jupyterlite-pyolite-kernel-extension-0.1.0-beta.7.tgz: 204589348f96e1ddd1a6bbad8c1bd7118dfb22ef1bb79d8eb86397877b771421
    
    jupyterlite-repl-extension-0.1.0-beta.7.tgz: 11916300b35431d66e0a883ddbebc82e6895fd6e08abb3a850267cdc010dd31c
    
    jupyterlite-retro-application-extension-0.1.0-beta.7.tgz: 6388396664ee57c93210c13044254d4df41e149d6fd3affc90cfde826d2e15c0
    
    jupyterlite-server-0.1.0-beta.7.tgz: 0a18b997731d33d3ced4f615580b5d1fb9bdc37ff2cdb7c7018974dffa3b9916
    
    jupyterlite-server-extension-0.1.0-beta.7.tgz: faa9dd347e16fe93906620c47077572f8e9d565dbe4e83f67d6e74eab343e6ae
    
    jupyterlite-session-0.1.0-beta.7.tgz: e3c8e2621c685e3994b76c3c7a6a6e0b03efd943e2521f4dd771caf6e227b749
    
    jupyterlite-settings-0.1.0-beta.7.tgz: d9a2abf8f7b52e1bfa6f4c9d75be0fb9bb93a4cbc8d7a329fe0083a4e9c613fb
    
    jupyterlite-translation-0.1.0-beta.7.tgz: 08e85e230e0385dbcf3864dc54636f116b5514a6cf823b74df59ab2a60afba2e
    
    jupyterlite-types-0.1.0-beta.7.tgz: 2c857b88a04cec20eb7616de4aa18a77ef922807886149cdbe52994ac26969aa
    
    jupyterlite-ui-components-0.1.0-beta.7.tgz: bdc24c921c83328999e4b55dfc828dd9a97be9c85e6d912d0aba476af19a61db

--- packages/pyolite-kernel/py/piplite/piplite/__init__.py
+++ packages/pyolite-kernel/py/piplite/piplite/__init__.py
@@ -1,6 +1,6 @@
 """A configurable Python package backed by Pyodide's micropip"""
 from .piplite import install
 
-__version__ = "0.1.0b6"
+__version__ = "0.1.0b7"
 
 __all__ = ["install"]
--- py/jupyterlite/src/jupyterlite/__init__.py
+++ py/jupyterlite/src/jupyterlite/__init__.py
@@ -8,7 +8,7 @@ VersionInfo = namedtuple(
 )
 
 # DO NOT EDIT THIS DIRECTLY!  It is managed by bumpversion
-version_info = VersionInfo(0, 1, 0, "beta", 6)
+version_info = VersionInfo(0, 1, 0, "beta", 7)
 
 _specifier_ = {"alpha": "a", "beta": "b", "candidate": "rc", "final": ""}
 
