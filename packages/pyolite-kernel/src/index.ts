a1e19538d37bcd62953ce0efedb41ed20c522cb7
Nicholas Bollweg <nick.bollweg@gmail.com>
2022-06-09 14:31:37 -0500

    hoist more in pyolite for downstreams

--- packages/pyolite-kernel/src/index.ts
+++ packages/pyolite-kernel/src/index.ts
@@ -1,4 +1,8 @@
 // Copyright (c) Jupyter Development Team.
 // Distributed under the terms of the Modified BSD License.
 
+export * from './_pypi';
+export * from './comlink.worker';
 export * from './kernel';
+export * from './tokens';
+export * from './worker';
