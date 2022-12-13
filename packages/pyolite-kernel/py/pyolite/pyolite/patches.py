462727c64ad6e150e38e3df9d8f8e797f2a500c1
Nicholas Bollweg <nick.bollweg@gmail.com>
2022-12-13 07:58:40 -0600

    remove PIL patch

--- packages/pyolite-kernel/py/pyolite/pyolite/patches.py
+++ packages/pyolite-kernel/py/pyolite/pyolite/patches.py
@@ -5,23 +5,7 @@ def patch_matplotlib():
         os.environ["MPLBACKEND"] = "module://matplotlib_inline.backend_inline"
 
 
-def patch_pillow():
-    import base64
-
-    from PIL import Image as PILImage
-
-    _old_repr_png = PILImage.Image._repr_png_
-    assert _old_repr_png
-
-    def _repr_png_(self):
-        byte = _old_repr_png(self)
-        return base64.b64encode(byte).decode("utf-8")
-
-    PILImage.Image._repr_png_ = _repr_png_
-
-
 ALL_PATCHES = [
-    patch_pillow,
     patch_matplotlib,
 ]
 
