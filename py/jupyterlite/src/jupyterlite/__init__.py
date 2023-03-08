eccd30245dae64e6dbf63cf17fb7133f63bf3418
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2023-03-08 09:18:24 +0000

    Add metapackage

---
+++ py/jupyterlite/src/jupyterlite/__init__.py
@@ -0,0 +1,7 @@
+"""re-export the core constants and addons from the core package.
+"""
+
+__version__ = "0.1.0b18"
+
+from jupyterlite_core.constants import *
+from jupyterlite_core.addons.base import *
