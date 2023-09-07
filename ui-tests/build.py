97fd05366521fd0ae067de6bd04ccbc8b58b4a6b
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2023-09-07 10:16:39 +0200

    Use sys.executable to build UI tests

--- ui-tests/build.py
+++ ui-tests/build.py
@@ -1,3 +1,5 @@
+import sys
+
 from pathlib import Path
 from subprocess import run
 
@@ -5,8 +7,9 @@ import jupyterlab
 
 extra_labextensions_path = str(Path(jupyterlab.__file__).parent / "galata")
 
+
 run(
-    f"jupyter lite build --FederatedExtensionAddon.extra_labextensions_path={extra_labextensions_path}",
+    f"{sys.executable} -m jupyterlite build --FederatedExtensionAddon.extra_labextensions_path={extra_labextensions_path}",
     shell=True,
     check=True,
 )
