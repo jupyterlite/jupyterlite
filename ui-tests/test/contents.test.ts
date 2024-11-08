a85745f9a241d954b3069d39530deb136cd3415f
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2024-11-08 21:19:02 +0100

    Update ui-tests/test/contents.test.ts

--- ui-tests/test/contents.test.ts
+++ ui-tests/test/contents.test.ts
@@ -27,11 +27,6 @@ test.use({
 
 test.describe('Contents Tests', () => {
   test.beforeEach(async ({ page }) => {
-    page.on('console', (message) => {
-      // eslint-disable-next-line no-console
-      console.log('CONSOLE MSG', message.text());
-    });
-
     await page.goto('lab/index.html');
   });
 
