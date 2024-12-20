ac4e4b53e3a6956f962a718910838fa45adc38b9
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2024-12-20 12:17:06 +0000

    add TODO

--- ui-tests/test/nojs.test.ts
+++ ui-tests/test/nojs.test.ts
@@ -6,6 +6,8 @@ import { test, expect } from '@playwright/test';
 test.describe('JavaScript disabled', () => {
   test.use({ javaScriptEnabled: false });
 
+  // TODO: re-enable
+  // See https://github.com/jupyterlite/jupyterlite/pull/1476#issuecomment-2556697378
   test.skip('Show a message on the page to enable JavaScript', async ({ page }) => {
     await page.goto('lab/index.html');
 
