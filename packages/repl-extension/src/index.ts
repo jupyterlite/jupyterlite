3262a8f074bf6ae21c04de7d0004af570df8c45e
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2025-01-01 13:27:03 +0100

    Allow not executing code with the repl (#1547)
    * Allow not executing code with the repl
    
    * add to the docs
    
    * add ui test
    
    * fix test
    
    * Add snapshots

--- packages/repl-extension/src/index.ts
+++ packages/repl-extension/src/index.ts
@@ -141,6 +141,7 @@ const consolePlugin: JupyterFrontEndPlugin<void> = {
     const search = window.location.search;
     const urlParams = new URLSearchParams(search);
     const code = urlParams.getAll('code');
+    const execute = urlParams.get('execute');
     const kernel = urlParams.get('kernel') || undefined;
     const theme = urlParams.get('theme')?.trim();
     const toolbar = urlParams.get('toolbar');
@@ -164,7 +165,12 @@ const consolePlugin: JupyterFrontEndPlugin<void> = {
 
       if (code) {
         await console.sessionContext.ready;
-        code.forEach((line) => console.inject(line));
+        if (execute === '0') {
+          const codeContent = code.join('\n');
+          console.replaceSelection(codeContent);
+        } else {
+          code.forEach((line) => console.inject(line));
+        }
       }
     });
   },
