84294fbfcf10ec1143dde6c8ea95b962b029c286
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2025-04-18 13:43:06 +0200

    Stabilize UI tests (#1628)
    * Stabilize UI tests
    
    * fix waitForApplication

--- ui-tests/test/notebook.test.ts
+++ ui-tests/test/notebook.test.ts
@@ -157,10 +157,10 @@ test.describe('Switch between Notebook and JupyterLab', () => {
   test.use({
     waitForApplication: async function ({ baseURL }, use, testInfo) {
       const waitIsReady = async (page): Promise<void> => {
-        const selector = page.url().includes('lab')
-          ? `text=${NOTEBOOK}`
-          : '.jp-NotebookPanel';
-        await page.waitForSelector(selector);
+        await Promise.race([
+          page.locator('.jp-NotebookPanel').waitFor(),
+          page.locator('.jp-Launcher').waitFor(),
+        ]);
       };
       await use(waitIsReady);
     },
