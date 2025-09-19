5ecde883e5950f5da7cae2ec51c57c5463d689f1
Ian Thomas <ianthomas23@gmail.com>
2025-09-19 07:30:47 +0100

    Throw EINVAL (28) in `DriveFSEmscriptenNodeOps.readlink` (#1723)
    * Throw EINVAL (28) in DriveFSEmscriptenNodeOps.readlink
    
    * Comment out new test
    
    * Reinstate new test
    
    * Use pyodide-kernel PR 221 wheel in ui-tests and examples

--- packages/contents/src/drivefs.ts
+++ packages/contents/src/drivefs.ts
@@ -425,7 +425,7 @@ export class DriveFSEmscriptenNodeOps implements IEmscriptenNodeOps {
   };
 
   readlink = (node: IEmscriptenFSNode | IEmscriptenStream): string => {
-    throw new this.fs.FS.ErrnoError(this.fs.ERRNO_CODES['EPERM']);
+    throw new this.fs.FS.ErrnoError(this.fs.ERRNO_CODES['EINVAR']);
   };
 }
 
