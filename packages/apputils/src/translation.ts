00ea83ec88506c77fa2c03fb6381276f8848e22e
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2026-03-10 10:57:40 +0100

    ESLint 9 (#1891)

--- packages/apputils/src/translation.ts
+++ packages/apputils/src/translation.ts
@@ -54,7 +54,7 @@ export class LiteTranslatorConnector
       }
       this._prevLocale = locale;
       return json;
-    } catch (e) {
+    } catch {
       if (locale) {
         return {
           data: {},
