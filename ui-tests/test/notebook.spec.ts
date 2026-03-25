650e978f6338be807357f021baea77c176c15598
martinRenou <martin.renou@gmail.com>
2026-03-25 16:59:48 +0100

    linting

--- ui-tests/test/notebook.spec.ts
+++ ui-tests/test/notebook.spec.ts
@@ -208,7 +208,7 @@ test.describe('Switch between Notebook and JupyterLab', () => {
   test('Open a notebook with query parameter does not reopen the notebook another time', async ({
     page,
   }) => {
-    await page.goto(`lab/index.html`);
+    await page.goto('lab/index.html');
 
     await page.notebook.open(NOTEBOOK);
     await page.notebook.runCellByCell();
