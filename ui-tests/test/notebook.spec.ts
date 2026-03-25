c710e7f54bd36517d787a02f077903e40b4a5cec
martinRenou <martin.renou@gmail.com>
2026-03-25 09:27:50 +0100

    Add ui-tests

--- ui-tests/test/notebook.spec.ts
+++ ui-tests/test/notebook.spec.ts
@@ -204,4 +204,20 @@ test.describe('Switch between Notebook and JupyterLab', () => {
 
     expect(notebookPage.url()).toContain('notebooks');
   });
+
+  test('Open a notebook with query parameter does not reopen the notebook another time', async ({
+    page,
+  }) => {
+    await page.goto(`lab/index.html`);
+
+    await page.notebook.open(NOTEBOOK);
+    await page.notebook.runCellByCell();
+    await page.notebook.save();
+
+    await page.goto(`lab/index.html?path=${NOTEBOOK}`);
+
+    const notebooks = page.locator('.jp-NotebookPanel');
+
+    await expect(notebooks).toHaveCount(1);
+  });
 });
