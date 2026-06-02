8cdd5e4693ee57afbef25ffb28fa8869df427b24
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2026-06-02 11:32:03 +0200

    UI tests HTML report (#1965)

--- ui-tests/playwright.config.js
+++ ui-tests/playwright.config.js
@@ -1,8 +1,19 @@
 const baseConfig = require('@jupyterlab/galata/lib/playwright-config');
 
+const blobOptions = process.env.BLOB_FILENAME
+  ? { fileName: process.env.BLOB_FILENAME }
+  : {};
+
 module.exports = {
   ...baseConfig,
   retries: 1,
+  tag: process.env.PLAYWRIGHT_TEST_TAG,
+  reporter: process.env.CI
+    ? [
+        ['blob', blobOptions],
+        ['json', { outputFile: 'test-results/report.json' }],
+      ]
+    : [['list'], ['html', { open: 'on-failure' }]],
   use: {
     acceptDownloads: true,
     appPath: '',
